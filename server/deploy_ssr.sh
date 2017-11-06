8bff85ebbf3035dea826f0ab516d4e154b1e3567
zhangjiangbin <zhangjiangbin@zhijiesong.com>
2017-11-06 09:21:15 +0800

    完善一键部署节点的脚本

--- server/deploy_ssr.sh
+++ server/deploy_ssr.sh
@@ -8,7 +8,7 @@
 install_ssr(){
 	cd /root/
 	echo 'SSR下载中...'
-	wget -c https://github.com/Hao-Luo/Others/raw/master/apps/ssr/ssr.tar.gz && tar -zxvf ssr.tar.gz && cd shadowsocksr &&  ./setup_cymysql.sh
+	wget -c https://github.com/ssrpanel/ssrpanel/raw/master/server/ssr-3.4.0.tar.gz && tar -zxvf ssr-3.4.0.tar.gz && cd shadowsocksr &&  ./setup_cymysql.sh
 	echo '开始配置节点连接信息...'
 	stty erase '^H' && read -p "数据库服务器地址:" mysqlserver
 	stty erase '^H' && read -p "数据库服务器端口:" port
@@ -17,14 +17,14 @@ install_ssr(){
 	stty erase '^H' && read -p "数据库密码:" pwd
 	stty erase '^H' && read -p "本节点ID:" nodeid
 	stty erase '^H' && read -p "本节点流量计算比例:" ratio
-	sed -i -e "s/server/$mysqlserver/g" usermysql.json
-	sed -i -e "s/3306/$port/g" usermysql.json
-	sed -i -e "s/ssrpanel/$database/g" usermysql.json
-	sed -i -e "s/username/$username/g" usermysql.json
-	sed -i -e "s/pwd/$pwd/g" usermysql.json
+	sed -i -e "s/server_host/$mysqlserver/g" usermysql.json
+	sed -i -e "s/server_port/$port/g" usermysql.json
+	sed -i -e "s/server_db/$database/g" usermysql.json
+	sed -i -e "s/server_user/$username/g" usermysql.json
+	sed -i -e "s/server_password/$pwd/g" usermysql.json
 	sed -i -e "s/nodeid/$nodeid/g" usermysql.json
-	sed -i -e "s/1.0/$ratio/g" usermysql.json
-	echo -e "配置完成!\n如果无法连上主数据库，请检查本机防火墙或者数据库防火墙!"
+	sed -i -e "s/noderatio/$ratio/g" usermysql.json
+	echo -e "配置完成!\n如果无法连上数据库，请检查本机防火墙或者数据库防火墙!\n请自行编辑user-config.json，配置节点加密方式、混淆、协议等"
 }
 
 open_bbr(){
