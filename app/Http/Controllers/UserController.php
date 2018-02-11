5c32f8f6549964337596d84f745baba888739cfd
zhangjiangbin <zhangjiangbin@zhijiesong.com>
2018-02-11 15:59:25 +0800

    日常开发

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -87,7 +87,7 @@ class UserController extends Controller
             $ssr_str .= $node->server . ':' . ($node->single ? $node->single_port : $user->port);
             $ssr_str .= ':' . ($node->single ? $node->single_protocol : $user->protocol) . ':' . ($node->single ? $node->single_method : $user->method);
             $ssr_str .= ':' . ($node->single ? $node->single_obfs : $user->obfs) . ':' . ($node->single ? $this->base64url_encode($node->single_passwd) : $this->base64url_encode($user->passwd));
-            $ssr_str .= '/?obfsparam=' . $this->base64url_encode($obfs_param);
+            $ssr_str .= '/?obfsparam=' . ($node->single ? '' : $this->base64url_encode($obfs_param));
             $ssr_str .= '&protoparam=' . ($node->single ? $this->base64url_encode($user->port . ':' . $user->passwd) : $this->base64url_encode($protocol_param));
             $ssr_str .= '&remarks=' . $this->base64url_encode($node->name);
             $ssr_str .= '&group=' . $this->base64url_encode('VPN');
