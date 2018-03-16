926414c4dfddc1d59f40b17281cf99afe891f07f
Administrator <admin>
2018-03-16 14:53:37 +0800

    优化在手机下访问界面问题

--- resources/views/admin/addGroup.blade.php
+++ resources/views/admin/addGroup.blade.php
@@ -51,7 +51,7 @@
                             </div>
                             <div class="form-actions">
                                 <div class="row">
-                                    <div class="col-md-offset-3">
+                                    <div class="col-md-12">
                                         <button type="submit" class="btn green"> 提 交</button>
                                     </div>
                                 </div>
