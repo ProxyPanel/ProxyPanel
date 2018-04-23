cb3dde4544bfe4f2c49c1e814bd9cd5c87f27e31
bingo <362357482@qq.com>
2018-04-24 00:04:40 +0800

    fix bug

--- sql/update/20180423.sql
+++ sql/update/20180423.sql
@@ -1,7 +1,7 @@
 -- 订单表字段改名
 ALTER TABLE `order`
 	CHANGE COLUMN `totalOriginalPrice` `origin_amount` INT(11) NOT NULL DEFAULT '0' COMMENT '订单原始总价，单位分' AFTER `coupon_id`,
-	CHANGE COLUMN `totalPrice` `amount` INT(11) NOT NULL DEFAULT '0' COMMENT '订单总价，单位分' AFTER `orgin_amount`;
+	CHANGE COLUMN `totalPrice` `amount` INT(11) NOT NULL DEFAULT '0' COMMENT '订单总价，单位分' AFTER `origin_amount`;
 
 
 
