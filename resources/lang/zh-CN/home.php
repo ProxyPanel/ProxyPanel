223928bd363adce78ff486a0c3565d2f2af85e89
aiden3434 <40372263+aiden3434@users.noreply.github.com>
2018-06-28 14:40:48 +0800

    Update home.php

--- resources/lang/zh-CN/home.php
+++ resources/lang/zh-CN/home.php
@@ -97,8 +97,8 @@ return [
 
     // 流量日志
     'traffic_log_tips'                => '提示：月流量统计不会统计当天，天流量统计不会统计当前小时；如果无统计数据，请检查定时任务是否正常。(每月1日和每日0时因为没有统计流量，不显示流量)',
-    'traffic_log_30days'              => '最近 30 天的流量使用情况',
-    'traffic_log_24hours'             => '最近 24 小时的流量使用情况',
+    'traffic_log_30days'              => '这个月的流量使用情况',
+    'traffic_log_24hours'             => '今天的流量使用情况',
     'traffic_log_keywords'            => '使用流量',
     'traffic_log_unit'                => '单位 / MB',
     'traffic_log_max'                 => '使用高峰',
