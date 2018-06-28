9b3b0725652ea2e851624033cc2bcfd0bccc0830
aiden3434 <40372263+aiden3434@users.noreply.github.com>
2018-06-28 12:07:44 +0800

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -112,7 +112,7 @@ class Controller extends BaseController
         $port = $config['min_port'];
         $deny_port = [1068, 1109, 1434, 3127, 3128, 3129, 3130, 3332, 4444, 5554, 6669, 8080, 8081, 8082, 8181, 8282, 9996, 17185, 24554, 35601, 60177, 60179]; // 不生成的端口
 
-        $exists_port = User::query()->where('port', '>=', $config['min_port'])->where('port', '<=', $config['max_port'])->pluck('port')->toArray();
+        $exists_port = User::query()->where('port', '>=', $config['min_port'])->pluck('port')->toArray();
         while (in_array($port, $exists_port) || in_array($port, $deny_port)) {
             $port = $port + 1;
         }
