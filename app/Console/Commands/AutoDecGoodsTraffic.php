da9ec4bfc4c02af22885873d8246145f9d7f9014
admin <admin@admin.com>
2018-08-23 18:12:53 +0800

    日常开发

--- app/Console/Commands/AutoDecGoodsTraffic.php
+++ app/Console/Commands/AutoDecGoodsTraffic.php
@@ -8,13 +8,13 @@ use App\Http\Models\Order;
 use App\Http\Models\User;
 use App\Http\Models\UserLabel;
 use App\Http\Models\GoodsLabel;
-
 use Log;
+use DB;
 
 class AutoDecGoodsTraffic extends Command
 {
     protected $signature = 'autoDecGoodsTraffic';
-    protected $description = '自动扣减用户到期流量包的流量';
+    protected $description = '自动扣减用户到期商品的流量';
 
     public function __construct()
     {
@@ -35,35 +35,43 @@ class AutoDecGoodsTraffic extends Command
                 $defaultLabels = explode(',', $config['initial_labels_for_user']);
             }
 
-            foreach ($orderList as $order) {
-                if (empty($order->user) || empty($order->goods)) {
-                    continue;
+            DB::beginTransaction();
+            try {
+                foreach ($orderList as $order) {
+                    Order::query()->where('oid', $order->oid)->update(['is_expire' => 1]);
+
+                    if (empty($order->user) || empty($order->goods)) {
+                        continue;
+                    }
+
+                    if ($order->user->transfer_enable - $order->goods->traffic * 1048576 <= 0) {
+                        User::query()->where('id', $order->user_id)->update(['u' => 0, 'd' => 0, 'transfer_enable' => 0]);
+                    } else {
+                        User::query()->where('id', $order->user_id)->decrement('transfer_enable', $order->goods->traffic * 1048576);
+                    }
+
+                    // 删除该商品对应用户的所有标签
+                    UserLabel::query()->where('user_id', $order->user->id)->delete();
+
+                    // 取出用户的其他商品带有的标签
+                    $goodsIds = Order::query()->where('user_id', $order->user->id)->where('oid', '<>', $order->oid)->where('status', 2)->where('is_expire', 0)->groupBy('goods_id')->pluck('goods_id')->toArray();
+                    $goodsLabels = GoodsLabel::query()->whereIn('goods_id', $goodsIds)->groupBy('label_id')->pluck('label_id')->toArray();
+
+                    // 生成标签
+                    $labels = array_values(array_unique(array_merge($goodsLabels, $defaultLabels))); // 标签去重
+                    foreach ($labels as $vo) {
+                        $userLabel = new UserLabel();
+                        $userLabel->user_id = $order->user->id;
+                        $userLabel->label_id = $vo;
+                        $userLabel->save();
+                    }
                 }
 
-                if ($order->user->transfer_enable - $order->goods->traffic * 1048576 <= 0) {
-                    User::query()->where('id', $order->user_id)->update(['u' => 0, 'd' => 0, 'transfer_enable' => 0]);
-                } else {
-                    User::query()->where('id', $order->user_id)->decrement('transfer_enable', $order->goods->traffic * 1048576);
-                }
+                DB::commit();
+            } catch (\Exception $e) {
+                \Log::error($this->description . '：' . $e);
 
-                // 删除该商品对应用户的所有标签
-                UserLabel::query()->where('user_id', $order->user->id)->delete();
-
-                // 取出用户的其他商品带有的标签
-                $goodsIds = Order::query()->where('user_id', $order->user->id)->where('oid', '<>', $order->oid)->where('status', 2)->where('is_expire', 0)->groupBy('goods_id')->pluck('goods_id')->toArray();
-                $goodsLabels = GoodsLabel::query()->whereIn('goods_id', $goodsIds)->groupBy('label_id')->pluck('label_id')->toArray();
-
-                // 生成标签
-                $labels = array_values(array_unique(array_merge($goodsLabels, $defaultLabels))); // 标签去重
-                foreach ($labels as $vo) {
-                    $userLabel = new UserLabel();
-                    $userLabel->user_id = $order->user->id;
-                    $userLabel->label_id = $vo;
-                    $userLabel->save();
-                }
-
-
-                Order::query()->where('oid', $order->oid)->update(['is_expire' => 1]);
+                DB::rollBack();
             }
         }
 
