81c79e5016bed71289797487e64de5a13b10d17f
admin <admin@admin.com>
2018-08-28 15:36:34 +0800

    日常开发

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -314,7 +314,7 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-                            <h4 class="modal-title">Scheme Links - {{$node->name}}</h4>
+                            <h4 class="modal-title">{{$node->name}}</h4>
                         </div>
                         <div class="modal-body">
                             <textarea class="form-control" rows="5" readonly="readonly">{{$node->ssr_scheme}}</textarea>
