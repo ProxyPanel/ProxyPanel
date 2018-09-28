82f82c23a6a0278b8470d19ca480fae74cb29116
Bruskyii Panda <30641000+ssrpanel@users.noreply.github.com>
2018-09-28 10:55:55 +0800

    Update 20180928.sql

--- sql/update/20180928.sql
+++ sql/update/20180928.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `ssrpanel_dev`.`ss_node` 
+ALTER TABLE `ss_node` 
 ADD COLUMN `is_tcp_check` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否开启检测: 0-不开启、1-开启' AFTER `ssh_port`,
 DROP PRIMARY KEY,
 ADD PRIMARY KEY (`id`, `single_obfs`) USING BTREE;
