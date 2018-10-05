1d0704c9825994069cb87eeccdc15ef51656e327
garhing <37818717+garhing@users.noreply.github.com>
2018-10-05 15:50:08 +0800

    首页增加公告栏

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -68,7 +68,8 @@ class UserController extends Controller
         $view['website_customer_service'] = self::$systemConfig['website_customer_service'];
         $view['is_push_bear'] = self::$systemConfig['is_push_bear'];
         $view['push_bear_qrcode'] = self::$systemConfig['push_bear_qrcode'];
-
+        $view['articleList'] = Article::query()->where('type', 2)->where('is_del', 0)->orderBy('sort', 'desc')->orderBy('id', 'desc')->limit(10)->paginate(15);
+        
         // 推广返利是否可见
         if (!Session::has('referral_status')) {
             Session::put('referral_status', self::$systemConfig['referral_status']);
