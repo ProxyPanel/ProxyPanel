1a71a253db4fb388ebd906a1197b9e073da346d6
garhing <37818717+garhing@users.noreply.github.com>
2018-10-31 16:06:55 +0800

    单个商品限购

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -935,7 +935,7 @@ class UserController extends Controller
             }
       
       	    //单个商品限购
-            if (!$goods->is_limit) {
+            if ($goods->is_limit == 1) {
                 $noneExpireOrderExist = Order::query()->where('status', '>=', 0)->where('user_id', $user['id'])->where('goods_id', $goods_id)->exists();
                 if ($noneExpireOrderExist) {
                     return Response::json(['status' => 'fail', 'data' => '', 'message' => '创建支付单失败：此商品每人限购1次']);
