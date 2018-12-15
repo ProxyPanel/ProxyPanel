dc2aff5bb1a6a0ff270bdb5a6942b930c3093e45
JMVoid <hkdollar@outlook.com>
2018-12-15 10:45:11 +0800

    add REDIRECT_HTTPS in  .env.example for open/close https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
+        if (env('REDIRECT_HTTPS', false)) {
             \URL::forceScheme('https');
         }
 
