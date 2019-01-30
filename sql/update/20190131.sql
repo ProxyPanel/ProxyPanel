e8cec077cc7ab2df825a3d9384cf9f9c5b17e1f7
yiheyang <yiheyang@apustech.cn>
2019-01-31 05:29:39 +0800

    添加升级sql

---
+++ sql/update/20190131.sql
@@ -0,0 +1,4 @@
+INSERT INTO `config` values ('84', 'is_f2fpay', 0);
+INSERT INTO `config` VALUES ('85', 'f2fpay_app_id', '');
+INSERT INTO `config` VALUES ('86', 'f2fpay_private_key', '');
+INSERT INTO `config` VALUES ('87', 'f2fpay_public_key', '');
