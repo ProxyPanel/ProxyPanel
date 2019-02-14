a784af9a988f6bfc079a8813fd2ebf4810e99d68
admin <admin@admin.com>
2019-02-14 15:29:49 +0800

    fix scripts

--- queue.sh
+++ queue.sh
@@ -3,7 +3,7 @@ ps -ef | grep queue:work | grep -v grep
 if [ $? -ne 0 ]
 then
     echo "start queue process successfully....."
-    nohup php artisan queue:work database --queue=default --timeout=60 --sleep=5 --tries=3 >> ./storage/logs/queue.log 2>&1 &
+    nohup php artisan queue:work database --queue=default --timeout=60 --sleep=5 --tries=3 >> ./queue.log 2>&1 &
 else
     echo "queue is running....."
 fi
