41334324b8a40681a8cad35f72993640bb280217
admin <admin@admin.com>
2019-02-22 15:01:44 +0800

    修正唯一入口

--- app/Http/Middleware/isSecurity.php
+++ app/Http/Middleware/isSecurity.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use App\Components\Helpers;
 use Closure;
 use Log;
-use Session;
+use Cache;
 
 class isSecurity
 {
@@ -21,13 +21,16 @@ class isSecurity
     {
         $ip = getClientIP();
         $code = $request->get('securityCode');
+        $cacheKey = 'SecurityLogin_' . ip2long($ip);
         $websiteSecurityCode = Helpers::systemConfig()['website_security_code'];
 
-        if ($websiteSecurityCode) {
+        if ($websiteSecurityCode && !Cache::has($cacheKey)) {
             if ($code != $websiteSecurityCode) {
                 Log::info("拒绝非安全入口访问(" . $ip . ")");
 
                 return response()->view('auth.error', ['message' => '请使用安全码从<a href="/login?securityCode=" target="_self">安全入口</a>访问']);
+            } else {
+                Cache::put($cacheKey, $ip, 120); // 缓存120分钟，因为每个session默认存活120分钟
             }
         }
 
