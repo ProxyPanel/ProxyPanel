7ee1d06b51499c42ea5f213f239056e0966a0eef
MIse <494347958@qq.com>
2019-03-21 17:16:52 +0800

    暂时解决 多人提到的 Class translator does not exist 错误

--- config/app.php
+++ config/app.php
@@ -143,7 +143,7 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        //Illuminate\Translation\TranslationServiceProvider::class, // 弃用自带多国语言包功能
+        Illuminate\Translation\TranslationServiceProvider::class, // 弃用自带多国语言包功能
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
