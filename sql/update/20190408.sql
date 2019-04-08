713da0904c7b9fe5b48f3090efaa092abd192181
Bruskyii Panda <30641000+ssrpanel@users.noreply.github.com>
2019-04-09 00:46:19 +0800

    Update 20190408.sql

--- sql/update/20190408.sql
+++ sql/update/20190408.sql
@@ -3,4 +3,4 @@ ALTER TABLE `ss_node`
   DROP COLUMN `is_udp`;
 
 ALTER TABLE `ss_node`
-  ADD COLUMN `is_transit` TINYINT(4) NOT NULL DEFAULT '0' COMMENT '是否中转节点：0-否、1-是' AFTER `is_udp`;
+  ADD COLUMN `is_transit` TINYINT(4) NOT NULL DEFAULT '0' COMMENT '是否中转节点：0-否、1-是' AFTER `is_nat`;
