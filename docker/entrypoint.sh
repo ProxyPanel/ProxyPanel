e52ca47ceab53557cdbd6155628349bc92d952b4
Colette Contreras <ColetteContreras@outlook.com>
2020-10-22 15:03:18 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,7 @@ set -m
 printenv | grep -v " " | sed 's/^\(.*\)$/export \1/g' > /root/container_env.sh
 printenv | grep -v " " > /root/env.txt
 
-bash /etc/wait-for-it.sh $DB_HOST:$DB_PORT -t 45
+/etc/wait-for-it.sh $DB_HOST:$DB_PORT -t 45
 
 sudo -u "www-data" mkdir -p storage/framework/{cache,sessions,testing,views}
 chmod -R 777 storage
