130e7ffd0bcb4e9cc0116dc6cef02abada492aa9
Colette Contreras <ColetteContreras@outlook.com>
2020-10-24 13:38:30 +0800

    Fix V2ray WebAPI (#74)

--- app/Http/Controllers/Api/WebApi/V2RayController.php
+++ app/Http/Controllers/Api/WebApi/V2RayController.php
@@ -14,6 +14,10 @@ class V2RayController extends BaseController
     {
         $node = Node::find($id);
         $nodeDv = NodeCertificate::whereDomain($node->v2_host)->first();
+        $tlsProvider = $node->tls_provider ?: sysConfig('v2ray_tls_provider');
+        if (! $tlsProvider) {
+            $tlsProvider = null;
+        }
 
         return $this->returnData('获取节点信息成功', 'success', 200, [
             'id'              => $node->id,
@@ -21,11 +25,11 @@ class V2RayController extends BaseController
             'speed_limit'     => $node->speed_limit,
             'client_limit'    => $node->client_limit,
             'push_port'       => $node->push_port,
-            'redirect_url'    => sysConfig('redirect_url'),
+            'redirect_url'    => (string) sysConfig('redirect_url'),
             'secret'          => $node->auth->secret,
             'key'             => $nodeDv->key ?? '',
             'pem'             => $nodeDv->pem ?? '',
-            'v2_license'      => sysConfig('v2ray_license'),
+            'v2_license'      => (string) sysConfig('v2ray_license'),
             'v2_alter_id'     => $node->v2_alter_id,
             'v2_port'         => $node->v2_port,
             'v2_method'       => $node->v2_method,
@@ -34,8 +38,9 @@ class V2RayController extends BaseController
             'v2_host'         => $node->v2_host,
             'v2_path'         => $node->v2_path,
             'v2_tls'          => $node->v2_tls ? true : false,
-            'v2_tls_provider' => $node->tls_provider ?: sysConfig('v2ray_tls_provider'),
-        ]);
+            'v2_tls_provider' => $tlsProvider,
+        ]
+        );
     }
 
     // 获取节点可用的用户列表
