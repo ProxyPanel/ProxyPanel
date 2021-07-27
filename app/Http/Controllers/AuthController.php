0927cd4e1c98f98d512fce383abfc6b9d42e9d69
兔姬桑 <monkeyblacktech97@gmail.com>
2021-07-27 20:11:46 +0800

    Fix active & reset password user account type error

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -401,9 +401,7 @@ class AuthController extends Controller
     {
         if ($request->isMethod('POST')) {
             // 校验请求
-            $validator = Validator::make($request->all(), [
-                'email' => 'required|email|exists:user,email',
-            ]);
+            $validator = Validator::make($request->all(), ['username' => 'required|'.(sysConfig('username_type') ?? 'email').'|exists:user,username']);
 
             if ($validator->fails()) {
                 return Redirect::back()->withInput()->withErrors($validator->errors());
@@ -509,7 +507,7 @@ class AuthController extends Controller
     public function activeUser(Request $request)
     {
         if ($request->isMethod('POST')) {
-            $validator = Validator::make($request->all(), ['email' => 'required|email|exists:user,email']);
+            $validator = Validator::make($request->all(), ['username' => 'required|'.(sysConfig('username_type') ?? 'email').'|exists:user,username']);
 
             if ($validator->fails()) {
                 return Redirect::back()->withInput()->withErrors($validator->errors());
