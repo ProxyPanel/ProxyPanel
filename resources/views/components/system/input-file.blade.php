7a0d0f4315268bc908887a4eb29e7b853e6e7e6c
兔姬桑 <monkeyblacktech97@gmail.com>
2021-09-08 22:23:26 +0800

    Fix form格式不正确导致图片上传失败的问题

--- resources/views/components/system/input-file.blade.php
+++ resources/views/components/system/input-file.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <label class="col-form-label col-md-3" for="{{$code}}">{{$title}}</label>
         <div class="col-md-8">
-            <input type="file" id="{{$code}}" data-plugin="dropify" data-default-file="{{asset($value ?? '/assets/images/default.png')}}"/>
+            <input type="file" name="{{$code}}" data-plugin="dropify" data-default-file="{{asset($value ?? '/assets/images/default.png')}}"/>
             <button type="submit" class="btn btn-success float-right mt-10"> 提 交</button>
         </div>
     </div>
